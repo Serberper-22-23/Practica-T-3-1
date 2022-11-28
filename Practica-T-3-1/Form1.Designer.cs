29ce5b930bf42eea2a8cd5c7c47595747a95900f
Serberper-22-23 <serberper@alu.edu.gva.es>
2022-11-28 21:09:40 +0100

    Subida del form

--- Practica-T-3-1/Form1.Designer.cs
+++ Practica-T-3-1/Form1.Designer.cs
@@ -28,13 +28,97 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			this.label1 = new System.Windows.Forms.Label();
+			this.txtTelegrama = new System.Windows.Forms.TextBox();
+			this.cbUrgente = new System.Windows.Forms.CheckBox();
+			this.txtPrecio = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.bCalcular = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(52, 29);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(39, 13);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Texto";
+			// 
+			// txtTelegrama
+			// 
+			this.txtTelegrama.Location = new System.Drawing.Point(55, 46);
+			this.txtTelegrama.Multiline = true;
+			this.txtTelegrama.Name = "txtTelegrama";
+			this.txtTelegrama.Size = new System.Drawing.Size(353, 215);
+			this.txtTelegrama.TabIndex = 1;
+			// 
+			// cbUrgente
+			// 
+			this.cbUrgente.AutoSize = true;
+			this.cbUrgente.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.cbUrgente.Location = new System.Drawing.Point(55, 321);
+			this.cbUrgente.Name = "cbUrgente";
+			this.cbUrgente.Size = new System.Drawing.Size(78, 17);
+			this.cbUrgente.TabIndex = 2;
+			this.cbUrgente.Text = "Urgente?";
+			this.cbUrgente.UseVisualStyleBackColor = true;
+			// 
+			// txtPrecio
+			// 
+			this.txtPrecio.Location = new System.Drawing.Point(99, 363);
+			this.txtPrecio.Name = "txtPrecio";
+			this.txtPrecio.Size = new System.Drawing.Size(100, 20);
+			this.txtPrecio.TabIndex = 3;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.Location = new System.Drawing.Point(56, 366);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(43, 13);
+			this.label2.TabIndex = 4;
+			this.label2.Text = "Coste:";
+			// 
+			// bCalcular
+			// 
+			this.bCalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.bCalcular.Location = new System.Drawing.Point(359, 355);
+			this.bCalcular.Name = "bCalcular";
+			this.bCalcular.Size = new System.Drawing.Size(75, 23);
+			this.bCalcular.TabIndex = 5;
+			this.bCalcular.Text = "Calcular";
+			this.bCalcular.UseVisualStyleBackColor = true;
+			this.bCalcular.Click += new System.EventHandler(this.bCalcular_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.bCalcular);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.txtPrecio);
+			this.Controls.Add(this.cbUrgente);
+			this.Controls.Add(this.txtTelegrama);
+			this.Controls.Add(this.label1);
+			this.Name = "Form1";
 			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox txtTelegrama;
+		private System.Windows.Forms.CheckBox cbUrgente;
+		private System.Windows.Forms.TextBox txtPrecio;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Button bCalcular;
 	}
 }
 
